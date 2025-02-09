89281ea0bdf35487f4d7f899c2f6739d0c55eb9a
david <dalogar0801@g.educaand.es>
2025-02-09 22:03:26 +0100

    entendiendo el service que maneja los modelos de evento para añadirlo o contar la categoria

--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -1,35 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Event } from '../model/event';
 
+// puede marcar este servicio en otros componentes u otros servicios 
 @Injectable({
   providedIn: 'root',
 })
+
 export class LoggerService {
-  private events: Event[] = [];
+  private events: Event[] = []; // almaceno los eventos del modelo 
 
-  constructor() {}
+  constructor() { }
 
-  // Método para agregar un evento
+  // funcion que el paso el parametro evento para aagregarlo
   addEvent(event: Event): void {
     this.events.push(event);
   }
 
-  // Método para obtener los eventos, con filtro opcional por tipo
+  // si se pasa categoria devuelve los de esa, sino devuelve todos
   getEvents(category?: 'log' | 'warn' | 'error'): Event[] {
     return category ? this.events.filter(event => event.categoria === category) : this.events;
   }
 
-  // Método para contar los eventos por tipo
-  getEventsCount(): { log: number; warn: number; error: number } { 
+  // luego de los : lo que ponemos es que devuelve esta funcion, en este caso 
+  // un objeto con el numero de log, el numero de warn y el numero de error
+  getEventsCount(): { log: number; warn: number; error: number } {
     return this.events.reduce(
       (counts, event) => {
-        // Asegurándonos de incrementar el conteo del tipo correspondiente
+        // lo suma segun el tipo detectado en el evento        
         if (counts[event.categoria] !== undefined) {
           counts[event.categoria]++;
         }
         return counts;
       },
-      { log: 0, warn: 0, error: 0 }  
+      { log: 0, warn: 0, error: 0 }
     );
   }
 }
